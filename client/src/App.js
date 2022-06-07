b79eef1505efb2d9472f438f9dfd655734701766
Andrew Midthun <midth002@gmail.com>
2022-06-07 16:46:48 -0500

    Added to header component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.scss';
-import Navbar from "./components/navbar/Navbar"
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header"
 
 function App() {
   return (
     <>
     <Navbar />
+    <Header />
     </>
   );
 }
---
+++ client/src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./header.scss"
+
+ const Header = () => {
+  return (
+    <div class="jumbotron d-flex mx-auto pt-50 w-50">
+              <div class="title">
+                <p>Hello, my name is</p>
+                <h2 id="name">Andrew Midthun.</h2>
+                <h2>I'm a full stack developer.</h2>
+    </div>
+    </div>
+  )
+}
+
+export default Header;
