9960bdf5adee10e692ba9d1d8866829edbdf1b70
Andrew Midthun <midth002@gmail.com>
2022-06-22 14:23:03 -0500

    Added email to contact component.

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,6 +5,7 @@ const Contact = () => {
   return (
     <div className="contact" id="contact-me">
         <div className="contact-title"><h2>Get In Touch</h2></div>
+        <div className='email-div'><a href='mailto: midthun23@gmail.com' className="email-link">midthun23@gmail.com</a></div>
         <div className="contact-description">
           <p>I am currently looking for a new opportunity. Whether you want to email me questions or connect with me, I will try my best to get back to you!</p>
         </div>
